7dd4a3a9b4d070ccde988ae521212a85bb90db5c
yooqty <yooqtty@gmail.com>
2026-04-22 21:59:12 +1000

    lab6 fix

--- labs/lab6/lab6.c
+++ labs/lab6/lab6.c
@@ -5,7 +5,7 @@ int main() {
     FILE *femboys = fopen("femboys.txt", "r");
     FILE *futanari = fopen("futanari.txt", "w");
 
-    char ln[20], n[20], fn[20];
+    char ln[50], n[50], fn[50];
     int year;
 
     while (fscanf(femboys, "%s %s %s %d", ln, n, fn, &year) == 4) {
