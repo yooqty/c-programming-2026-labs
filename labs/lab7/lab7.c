ca29bfa36e4d932fa22ed321fc14b1b25c017586
yooqty <yooqtty@gmail.com>
2026-05-15 09:56:53 +1000

    lab7

--- labs/lab7/lab7.c
+++ labs/lab7/lab7.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define count 50
+#define cnt 50
 
 typedef struct {
     char n[50], fn[50], ln[50];
@@ -8,18 +8,22 @@ typedef struct {
 } humen;
 
 int main() {
-    humen a[count], b[count];
+    humen a[cnt], b[cnt];
     int i, j;
     humen temp;
 
     FILE *file = fopen("humen.txt", "r");
-    for (i=0; i<count; i++) {
+    for (i=0; i<cnt; i++) {
         fscanf(file, "%s", a[i].n);
         fscanf(file, "%s", a[i].fn);
         fscanf(file, "%s", a[i].ln);
         fscanf(file, "%d", a[i].n);
     }
     fclose(file);
+
+    for (i=0; i<cnt; i++) {
+        b[i] = a[i];
+    }
 
     return 0;
 }
