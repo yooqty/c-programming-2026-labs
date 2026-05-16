3931d2341083e786c3e7e8c65ec68e377afbe4c1
yooqty <yooqtty@gmail.com>
2026-05-16 13:10:40 +1000

    lab5 & lab7

---
+++ all.h
@@ -0,0 +1,10 @@
+# ifndef ALL_H
+# define ALL_H
+
+double area(double a, double b, double c);
+
+double perimetr(double a, double b, double c);
+
+int triangle(double a, double b, double c);
+
+# endif
