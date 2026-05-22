63c0c2c819f635c1e93daab3810d8f2561a6b308
yooqty <yooqtty@gmail.com>
2026-05-22 11:11:31 +1000

    lab8 compare people

--- labs/lab8/lab8.c
+++ labs/lab8/lab8.c
@@ -10,7 +10,7 @@ typedef struct {
 int sf[10];
 int cnt=0;
 
-int compare_fields(const void * a, const void * b) {
+int compare_people(const void * a, const void * b) {
     person * p1 = ( person *) a;
     person * p2 = ( person *) b;
     int i;
@@ -44,15 +44,21 @@ int main() {
     FILE *f = fopen("femboys.txt", "r");
     person people[100];
 
-    int cunt=0;
+    int count=0;
     char ln[50], fn[50];
-    while (fscanf(f, " %s %s %d %s %f", ln, fn, & people[cunt].year, people[cunt].g, & people[cunt].h) == 5) {
-        sprinf(people[cnt].n, "%s %s", ln, fn);
-        cunt++;
+    while (fscanf(f, " %s %s %d %s %f", ln, fn, & people[count].year, people[count].g, & people[count].h) == 5) {
+        sprinf(people[count].n, "%s %s", ln, fn);
+        count++;
     }
     fclose(f);
     
-
+    printf("Sort by: n(name/last name), y(birth year), g(gender), h(height)\n");
+    cnt=0;
+    int input;
+    while (scanf("%d", & input)==1 && input!=0 && cnt<10) {
+        sf[cnt++] = input;
+    }
+    qsort(people, count, sizeof(person), compare_people);
 
 
 
