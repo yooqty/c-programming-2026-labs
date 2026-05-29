35bd5b5685254fa19fe83bafe7a02d25704cab75
yooqty <yooqtty@gmail.com>
2026-05-29 20:11:47 +1000

    lab10 max

--- labs/lab10/lab10.c
+++ labs/lab10/lab10.c
@@ -2,14 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX(array, n) ({ \
-    int max = (array)[0];\
-    for (int i = 1; i < (n); i++){\
-       if ((array)[i]> max)\
-          max = (array)[i];\
-    }\
-    max;\
-})
+#define MAX(array, n) ({int max = (array)[0]; for (int i = 1; i < (n); i++) {if ((array)[i] > max) max = (array)[i];} max;})
 
 int main() {
     char input[10000];
@@ -31,6 +24,7 @@ int main() {
     }
 
     int max = MAX(numbers,cnt);
+
     printf(" Max number: %d", max);
 
     return 0;
