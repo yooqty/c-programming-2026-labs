b1d55f136501198fb48670751a0aee6f52c31fc2
yooqty <yooqtty@gmail.com>
2026-06-01 11:18:50 +1000

    lab11 functions.c

--- labs/lab11/functions.c
+++ labs/lab11/functions.c
@@ -2,3 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
+int sum(int n, ...) {
+    int res = 0;
+    va_list list;
+    va_start(list, n);
+    while(n)
+    {
+        res += va_arg(list, int);
+        --n;
+    }
+    va_end(list);
+    return res;
+}
