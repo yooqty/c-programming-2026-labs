8867566e29edd344e03e4091917bbc44647bf270
yooqty <yooqtty@gmail.com>
2026-06-15 22:49:06 +1000

    1.c

--- labs/exam1/1.c
+++ labs/exam1/1.c
@@ -7,4 +7,6 @@ int days_until_date(int day, int month, int year) {
 
     int cur_day = 13, cur_month = 6, cur_year = 2026;
 
+    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 }
