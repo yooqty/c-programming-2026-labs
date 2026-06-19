d49bf5a429a8f68df25cf155bf5a21d3a0418ad6
yooqty <yooqtty@gmail.com>
2026-06-19 15:54:28 +1000

    exam1 version2

--- labs/exam1/version2/version2.c
+++ labs/exam1/version2/version2.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+int** allocateMatrix(int rows, int cols) {
+    int** mat = (int**)malloc(rows * sizeof(int*));
+    for (int i = 0; i < rows; i++) {
+        mat[i] = (int*)malloc(cols * sizeof(int));
+    }
+    return mat;
+}
