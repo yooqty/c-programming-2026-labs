19fa977340a824ef72ac7073eca29693ab074d4e
yooqty <yooqtty@gmail.com>
2026-06-30 16:37:35 +1000

    exam2 version2

--- labs/exam2/version2.c
+++ labs/exam2/version2.c
@@ -118,8 +118,9 @@ void findMinElement(int** mat, int rows, int cols, int* minRow, int* minCol) {
     *minRow = 0;
     *minCol = 0;
     int minVal = mat[0][0];
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    int i,j;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
             if (mat[i][j] < minVal) {
                 minVal = mat[i][j];
                 *minRow = i;
